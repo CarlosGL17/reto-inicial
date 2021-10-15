383412242593b05b05b27c386e19dc6799f4e88d
CarlosGL17 <cev5460@innovaccion.com>
2021-10-14 21:16:59 -0500

    Test uikit

---
+++ components/Wallpaper.tsx
@@ -0,0 +1,12 @@
+import Portada from '../assets/img/Newport_Whitepit_Lane_pot_hole-min.webp';
+
+const Wallpaper = () =>
+<div>
+    <div className='image'
+        style={{backgroundImage: `url(${Portada})`}}
+    >
+        <h1 className='uk-heading-medium'>¡BIENVENIDO!</h1>
+    </div>
+</div>
+
+export default Wallpaper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import Wallpaper from '../components/Wallpaper';
+
 const myApp = () =>
 <div>
-    <h1 className='uk-heading'>Hola mundo!</h1>
+    <Wallpaper/>
 </div>
 
 export default myApp;
